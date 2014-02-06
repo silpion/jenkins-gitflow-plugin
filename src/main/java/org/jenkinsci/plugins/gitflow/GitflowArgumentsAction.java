4122b468368415154182197d5bfbfe441c8bdb21
Marc Rohlfs <rohlfs@silpion.de>
2014-02-06 12:19:46 +0100

    Plugin development should be done with the latest stable version of Jenkins and its plugins.

---
+++ src/main/java/org/jenkinsci/plugins/gitflow/GitflowArgumentsAction.java
@@ -0,0 +1,10 @@
+package org.jenkinsci.plugins.gitflow;
+
+/**
+ * TODO Write Javadoc
+ *
+ * @author Marc Rohlfs, Silpion IT-Solutions GmbH - rohlfs@silpion.de
+ */
+public class GitflowArgumentsAction {
+
+}
