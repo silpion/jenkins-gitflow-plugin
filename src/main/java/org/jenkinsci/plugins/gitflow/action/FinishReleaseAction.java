20fbd586623105796be91f2f21a2d0e1973652ab
Marc Rohlfs <rohlfs@silpion.de>
2014-07-27 19:26:58 +0200

    Feature #10375: Unification of methods to create and delete a Git branch for 'Publish Release' and 'Finish Release' actions.

--- src/main/java/org/jenkinsci/plugins/gitflow/action/FinishReleaseAction.java
+++ src/main/java/org/jenkinsci/plugins/gitflow/action/FinishReleaseAction.java
@@ -1,24 +1,15 @@
 package org.jenkinsci.plugins.gitflow.action;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.text.MessageFormat;
-import java.util.Collection;
 
 import org.apache.commons.lang.StringUtils;
-import org.eclipse.jgit.transport.URIish;
 import org.jenkinsci.plugins.gitflow.GitflowBuildWrapper;
 import org.jenkinsci.plugins.gitflow.cause.FinishReleaseCause;
-import org.jenkinsci.plugins.gitflow.data.RemoteBranch;
 import org.jenkinsci.plugins.gitflow.gitclient.GitClientDelegate;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
 
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
-import hudson.plugins.git.Branch;
 
 /**
  * This class executes the required steps for the Gitflow action <i>Finish Release</i>.
@@ -30,18 +21,6 @@ public class FinishReleaseAction<B extends AbstractBuild<?, ?>> extends Abstract
 
     private static final String ACTION_NAME = "Finish Release";
     private static final String MSG_PREFIX = "Gitflow - " + ACTION_NAME + ": ";
-    private static final MessageFormat MSG_PATTERN_NEW_HOTFIX_BRANCH_BASED_ON_RELEASE = new MessageFormat(MSG_PREFIX + "Created a new branch {0} based on {1}");
-    private static final MessageFormat MSG_PATTERN_DELETED_BRANCH = new MessageFormat(MSG_PREFIX + "Deleted branch {0}");
-
-    private static final Function<Branch, String> BRANCH_TO_NAME_FUNCTION = new Function<Branch, String>() {
-
-        /** {@inheritDoc} */
-        public String apply(final Branch input) {
-            return input == null ? null : input.getName();
-        }
-    };
-
-    private final URIish remoteUrl;
 
     /**
      * Initialises a new <i>Finish Release</i> action.
@@ -57,13 +36,6 @@ public class FinishReleaseAction<B extends AbstractBuild<?, ?>> extends Abstract
     public <BC extends B> FinishReleaseAction(final BC build, final Launcher launcher, final BuildListener listener, final GitClientDelegate git, final FinishReleaseCause gitflowCause)
             throws IOException, InterruptedException {
         super(build, launcher, listener, git, gitflowCause);
-
-        // Create remote URL.
-        try {
-            this.remoteUrl = new URIish("origin");
-        } catch (final URISyntaxException urise) {
-            throw new IOException("Cannot create remote URL", urise);
-        }
     }
 
     /** {@inheritDoc} */
@@ -98,42 +70,6 @@ public class FinishReleaseAction<B extends AbstractBuild<?, ?>> extends Abstract
 
         // Abort the job, because there's no need to execute the main build.
         this.omitMainBuild();
-    }
-
-    private void createBranch(final String newBranchName, final String releaseBranch) throws InterruptedException {
-
-        // Create a new hotfix branch.
-        this.git.checkoutBranch(newBranchName, "origin/" + releaseBranch);
-        this.consoleLogger.println(formatPattern(MSG_PATTERN_NEW_HOTFIX_BRANCH_BASED_ON_RELEASE, newBranchName, releaseBranch));
-
-        // Push the new hotfix branch.
-        this.git.push().to(this.remoteUrl).ref("refs/heads/" + newBranchName + ":refs/heads/" + newBranchName).execute();
-
-        // Record the data for the new remote branch.
-        final RemoteBranch remoteBranchRelease = this.gitflowPluginData.getRemoteBranch("origin", releaseBranch);
-        final RemoteBranch remoteBranchNew = this.gitflowPluginData.getOrAddRemoteBranch("origin", newBranchName);
-        remoteBranchNew.setLastBuildResult(remoteBranchRelease.getLastBuildResult());
-        remoteBranchNew.setLastBuildVersion(remoteBranchRelease.getLastBuildVersion());
-        remoteBranchNew.setLastReleaseVersion(remoteBranchRelease.getLastReleaseVersion());
-        remoteBranchNew.setLastReleaseVersionCommit(remoteBranchRelease.getLastReleaseVersionCommit());
-    }
-
-    private void deleteBranch(final String branchName) throws InterruptedException {
-
-        // Delete the remote branch locally and remotely.
-        final Collection<String> localBranches = Collections2.transform(this.git.getBranches(), BRANCH_TO_NAME_FUNCTION);
-        if (localBranches.contains(branchName)) {
-            // The local branch might be missing when the action was executed in 'Dry Run' mode before.
-            this.git.deleteBranch(branchName);
-        }
-        this.consoleLogger.println(formatPattern(MSG_PATTERN_DELETED_BRANCH, branchName));
-        this.git.push().to(this.remoteUrl).ref(":refs/heads/" + branchName).execute();
-
-        // Remove the recorded data of the deleted remote branch.
-        final RemoteBranch remoteBranch = this.gitflowPluginData.getRemoteBranch("origin", branchName);
-        if (remoteBranch != null) {
-            this.gitflowPluginData.removeRemoteBranch(remoteBranch, false);
-        }
     }
 
     /** {@inheritDoc} */
